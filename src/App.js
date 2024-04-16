84db174b3f4eaefb199b6171c23797d2306f1224
regent-ndongmo <jauressndongmo5287@gmail.com>
2024-04-16 15:54:27 +0100

    Modification du travail de nicaise 16/01/2024

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import Accueil from './component/Accueil-component/Accueil';
+import Layout from './component/Layout/Layout';
+import Container from './component/Container';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      
-    </div>
+      <Layout />
+      <Container />
+     </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Accueil from './component/Accueil-component/Accueil';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Accueil/>
+    <App />
   </React.StrictMode>
 );
 
